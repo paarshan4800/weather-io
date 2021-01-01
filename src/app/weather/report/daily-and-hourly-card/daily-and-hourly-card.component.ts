ecb15f5eacd487470aec284b1c5a1709bd580f18
Paargav Shanker <56292759+paarshan4800@users.noreply.github.com>
2021-01-01 22:36:54 +0530

    fixed few bugs in design

--- src/app/weather/report/daily-and-hourly-card/daily-and-hourly-card.component.ts
+++ src/app/weather/report/daily-and-hourly-card/daily-and-hourly-card.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-daily-and-hourly-card',
   templateUrl: './daily-and-hourly-card.component.html',
-  styleUrls: ['./daily-and-hourly-card.component.css']
+  styleUrls: ['./daily-and-hourly-card.component.css'],
+  host: {
+    "(window:resize)": "onWindowResize($event)"
+  }
 })
 export class DailyAndHourlyCardComponent implements OnInit {
   @Input() report;
@@ -12,9 +15,23 @@ export class DailyAndHourlyCardComponent implements OnInit {
   @Input() locationDetails;
   @Input() isHourly;
 
+  mobileWidth: number = 468;
+  xsmobileWidth: number = 350;
+  width: number = window.innerWidth;
+  height: number = window.innerHeight;
+  isMobile: boolean;
+  isXsMobile: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onWindowResize(event) {
+    this.width = event.target.innerWidth;
+    this.height = event.target.innerHeight;
+    this.isMobile = this.width < this.mobileWidth;
+    this.isXsMobile = this.width < this.xsmobileWidth;
   }
 
 
